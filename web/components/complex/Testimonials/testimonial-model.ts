6dd9eb0cece42fc7cef552432d73d2b8da63e56e
Omer Hamid Kamisli <me@ohkamisli.dev>
2024-10-26 02:27:23 -0700

    fix(testimonials): correct URL in mock testimonials to resolve compile error
    - Updated one of the background image URLs in mockTestimonials
    - This change resolves a compilation issue related to the invalid URL

--- web/components/complex/Testimonials/testimonial-model.ts
+++ web/components/complex/Testimonials/testimonial-model.ts
@@ -15,7 +15,7 @@ export type { Testimonial };
 const mockTestimonials: Testimonial[] = [
     {
         id: "1",
-        background: "https://unsplash.com/photos/a-woman-standing-on-a-beach-next-to-the-ocean-c93Lw07ak64",
+        background: "https://images.unsplash.com/photo-1729218620890-f120489dcd11?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         testimonial: "This product has revolutionized our workflow!",
         author: {
             name: "John Doe",
